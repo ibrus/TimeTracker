4ce818f93d1abe8f237d2c54b1e9c6fbdda785bd
ibrus <brus.andreevich@gmail.com>
2020-09-22 22:02:40 +0300

    some field added

--- src/main/java/com/timetracker/model/Worker.java
+++ src/main/java/com/timetracker/model/Worker.java
@@ -2,11 +2,12 @@ package com.timetracker.model;
 
 import javax.persistence.Entity;
 import java.time.LocalDate;
+import java.util.LinkedList;
 import java.util.List;
 
 @Entity
 public class Worker extends Employee{
 
     private Supervisor supervisor;
-    private List<LocalDate> punches;
+    private List<LocalDate> punches = new LinkedList<>();
 }
